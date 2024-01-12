52fc17f8f5dae63620b803bd1e5152520b53cea6
karishmaMohammed <86139881+karishmaMohammed@users.noreply.github.com>
2024-01-12 18:09:11 +0530

    Update contant.js

--- src/contant.js
+++ src/contant.js
@@ -1,3 +1,3 @@
 export const PREFIX_URL = "https://jwelleries.s3.ap-south-1.amazonaws.com/"
 export const ASSETS_URL = "https://portfolio-ks.s3.ap-south-1.amazonaws.com/"
-export const BASE_URL = "http://localhost:7000/user/"
+export const BASE_URL = "https://karishma-jewellers-diamonds.onrender.com/user/"
